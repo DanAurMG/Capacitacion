83e94a20f11924e7787a6b3135823fb333603ca3
DanAurMG <raizeremi.15.damg@gmail.com>
2025-04-07 23:17:31 -0600

    DAMG 07-04-2025: add current page to pagination

--- src/components/PaginationList.vue
+++ src/components/PaginationList.vue
@@ -1,43 +1,43 @@
 <template>
-    <div class="pagination-list">
-      <p>Mover esto hasta arriba</p>
-      <button 
-        :disabled="!info.prev" 
-        @click="changePage(info.prev)">
-        Prev
-      </button>
-      <span>Page {{ info.page }} of {{ info.pages }}</span>
-      <button 
-        :disabled="!info.next" 
-        @click="changePage(info.next)">
-        Next
-      </button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'PaginationList',
-    props: {
-      info: Object
-    },
-    methods: {
-      changePage(page) {
-        console.log("Nueva pagina", page);
-        
-        this.$emit('change-page', page)
+  <div class="pagination-list">
+    <p>Mover esto hasta arriba</p>
+    <button :disabled="!info.prev" @click="changePage(info.prev)">
+      Prev
+    </button>
+    <span>Page {{ getCurrentPage()}} of {{ info.pages }}</span>
+    <button :disabled="!info.next" @click="changePage(info.next)">
+      Next
+    </button>
+  </div>
+</template>
 
-      }
+<script>
+export default {
+  name: 'PaginationList',
+  props: {
+    info: Object
+  },
+  methods: {
+    changePage(page) {
+      console.log("Nueva pagina", page);
+
+      this.$emit('change-page', page)
+
+    }, getCurrentPage() {
+      const prevUrl = this.info?.prev;
+      if (!prevUrl) return 1;
+      const url = new URL(prevUrl);
+      return parseInt(url.searchParams.get('page')) + 1;
     }
   }
-  </script>
-  
-  <style scoped>
-  .pagination {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+</style>
