3f57d583b67afc23eb7d4f1321d2c9cdf1720dca
Daniel <dmunoz@iceiot.com.mx>
2025-04-08 18:07:57 -0600

    DAMG 08-04-2025: Done with axios Should change axios for grapghql X.x

--- src/components/FilterSection.vue
+++ src/components/FilterSection.vue
@@ -2,25 +2,35 @@
     <div class="filter">
       <p>Total de personajes:</p>
       <p>{{ info.count }}</p>
-      <input v-model="filter.charName" placeholder="Search by name"  v-on:keyup.enter = "searchName" v-on:keyup = "searchName"/>
-      <button @click="searchName">Buscar nombre</button>
+      <p>¡Busca por nombre!</p>
+      <input v-model="filter.name" placeholder="Search by name"  v-on:keyup.enter = "searchName"/>
+
+      <p>¡O aplica alguno de los filtros!</p>
       <select v-model="filter.status">
         <option value="">All Status</option>
         <option value="Alive">Alive</option>
         <option value="Dead">Dead</option>
         <option value="unknown">Unknown</option>
       </select>
-      <select name="" id="">
+      <select v-model="filter.species">
         <option value="">All species</option>
         <option value="Alien">Alien</option>
         <option value="Human">Human</option>
         <option value="Robot">Robot</option>
+      </select>
+      <select v-model="filter.gender">
+        <option value="">All species</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
       </select>
       <button @click="applyFilter">Apply</button>
     </div>
   </template>
   
   <script>
+      //<input v-model="filter.name" placeholder="Search by name"  v-on:keyup.enter = "searchName" v-on:keyup = "searchName"/>
+
+
   export default {
     name: 'FilterSection',
     props:{
@@ -30,7 +40,9 @@
       return {
         filter: {
           name: '',
-          status: ''
+          status: '',
+          species: '',
+          gender: '',
         }
       }
     },
@@ -39,7 +51,7 @@
         this.$emit('update-filter', { ...this.filter })
       },
       searchName(){
-        const character = this.filter.charName;
+        const character = this.filter.name;
         this.$emit('search-name', character);
       }
 
--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,107 +1,229 @@
 <template>
-    <div class="app">
-      <div class="filtering">
-        <filter-section @update-filter="applyFilter" @search-name="filterName" :info="paginationInfo"/>
-      </div>
-      <div class="listing">
-        <pagination-list 
-        :info="paginationInfo"
-        @change-page="fetchCharacters" />
-        <character-list :characters="characters" />
-      </div>
+  <div class="app">
+    <div class="filtering">
+      <filter-section @update-filter="applyFilter" @search-name="filterName" :info="paginationInfo" />
     </div>
-  </template>
-  
-  <script>
-  import CharacterList from '@/components/CharacterList.vue'
-  import PaginationList from '@/components/PaginationList.vue'
-  import FilterSection from '@/components/FilterSection.vue'
-  import axios from 'axios'
-  
-  export default {
-    name: 'MainView',
-    components: {
-      CharacterList,
-      PaginationList,
-      FilterSection
-    },
-    data() {
-      return {
-        characters: [],
-        filteredCharacters: [],
-        paginationInfo: {},
-        filters: {}
+    <div class="listing">
+      <pagination-list :info="paginationInfo" @change-page="fetchCharacters" />
+      <character-list :characters="characters" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CharacterList from '@/components/CharacterList.vue'
+import PaginationList from '@/components/PaginationList.vue'
+import FilterSection from '@/components/FilterSection.vue'
+import axios from 'axios'
+
+export default {
+  name: 'MainView',
+  components: {
+    CharacterList,
+    PaginationList,
+    FilterSection
+  },
+  data() {
+    return {
+      characters: [],
+      filteredCharacters: [],
+      paginationInfo: {},
+      filters: {}
+    }
+  },
+  mounted() {
+    this.fetchCharacters()
+  },
+  methods: {
+    async fetchCharacters(page) {
+      try {
+        //https://rickandmortyapi.com/graphql
+        const url = !page ? "https://rickandmortyapi.com/api/character/?page=1" : page;
+        const axios = require("axios")
+        //No puedo pasar los parámetros como ${variable}
+
+        axios({
+          url: "https://rickandmortyapi.com/graphql",
+          method: 'post',
+          data: {
+            query: `
+              query PostsForChars() {
+                  characters(filter: {}) {
+                    info {
+                      count,
+                      next,
+                      pages,
+                      prev,
+                    }
+                    results {
+                      name,
+                      species,
+                      status,
+                      gender,
+                      image,
+                      location{
+                        name,
+                      }
+
+                    }
+                }
+              }
+            `
+          }
+        }).then((result) => {
+          console.log("Tus datos: ", result.data);
+        });
+
+        const response = await axios.get(`${url}`)
+        console.log("datos con axios", response.data);
+        console.log(response.data.results);
+        
+        this.characters = response.data.results
+        this.paginationInfo = response.data.info
+      } catch (error) {
+        console.error(error)
       }
     },
-    mounted() {
-      this.fetchCharacters()
+    async applyFilter(filters) {
+
+      const params = {
+        status: filters.status,
+        species: filters.species,
+        gender: filters.gender,
+        name: filters.name,
+      }
+
+      try {
+
+
+        axios({
+          url: "https://rickandmortyapi.com/graphql",
+          method: 'post',
+          data: {
+            query: `
+              query PostsForChars {
+                  characters(filter: { name: ${filters.name}, status: ${filters.status}, species: ${filters.species}, gender: ${filters.gender}}) {
+                    info {
+                      count,
+                      next,
+                      pages,
+                      prev,
+                    }
+                    results {
+                      name,
+                      species,
+                      status,
+                      gender,
+                      image,
+                      location{
+                        name,
+                      }
+
+                    }
+                }
+              }
+            `
+          }
+        }).then((result) => {
+          console.log("Tus datos filtrados: ", result.data);
+        });
+
+
+        const response = await axios.get("https://rickandmortyapi.com/api/character/", { params });
+        this.characters = response.data.results;
+        this.paginationInfo = response.data.info;
+        console.log(this.characters);
+
+      } catch (error) {
+        console.log(error);
+      }
     },
-    methods: {
-      async fetchCharacters(page) {
-        try {
-          const url = !page ? "https://rickandmortyapi.com/api/character/?page=1" : page;
-          const response = await axios.get(`${url}`)
-          this.characters = response.data.results
-          this.paginationInfo = response.data.info
-        } catch (error) {
-          console.error(error)
-        }
-      },
-      async applyFilter() {
-        console.log(this.filters.name);
-        
-        let filtered = this.characters
-        if (this.filters.name) {
-          filtered = filtered.filter(character => character.name.toLowerCase().includes(this.filters.name.toLowerCase()))
-        }
-        if (this.filters.status) {
-          filtered = filtered.filter(character => character.status.toLowerCase() === this.filters.status.toLowerCase())
-        }
-        this.filteredCharacters = filtered
-      },
-      async filterName(characterName){
+    async filterName(characterName) {
+      try {
+
+        axios({
+          url: "https://rickandmortyapi.com/graphql",
+          method: 'post',
+          data: {
+            query: `
+              query PostsForChars {
+                  characters(filter: {name: ${characterName}}) {
+                    info {
+                      count,
+                      next,
+                      pages,
+                      prev,
+                    }
+                    results {
+                      name,
+                      species,
+                      status,
+                      gender,
+                      image,
+                      location{
+                        name,
+                      }
+
+                    }
+                }
+              }
+            `
+          }
+        }).then((result) => {
+          console.log("Tus datos por nombre: ", result.data);
+        });
+
+
         const response = await axios.get(`https://rickandmortyapi.com/api/character/?name=${characterName}`);
         this.characters = response.data.results;
         this.paginationInfo = response.data.info;
+        console.log(this.characters);
+
+      } catch (err) {
+        console.log(err);
+
       }
     }
   }
-  </script>
-  
-  
-  <style>
-  body{
-    padding: 0px;
-    margin: 0px;
-  }
-  
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 0px;
-    padding: 0px;
-    margin: 0px;
-   
-  }
-  .app{
-    display: flex;
-    flex-direction: row;
-    gap: 60px;
-    background-color: #FFFCF7;
-  }
-  .filtering{
-    padding: 20px;
-    font-size: larger;
-    background-color: #A1B5D8;
-  }
-  
-  .listing{
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    padding: 20px 0px 40px 0px;
-  }
-  </style>
+}
+</script>
+
+
+<style>
+body {
+  padding: 0px;
+  margin: 0px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+  padding: 0px;
+  margin: 0px;
+
+}
+
+.app {
+  display: flex;
+  flex-direction: row;
+  gap: 60px;
+  background-color: #FFFCF7;
+}
+
+.filtering {
+  padding: 20px;
+  font-size: larger;
+  background-color: #A1B5D8;
+}
+
+.listing {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px 0px 40px 0px;
+}
+</style>
